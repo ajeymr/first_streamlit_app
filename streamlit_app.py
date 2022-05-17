a45a3be5b0afb11cc50c208528a229c00e0b0ccd
ajeymr <52828678+ajeymr@users.noreply.github.com>
2022-05-17 11:08:44 -0700

    Update streamlit_app.py
    updating the items to show only the selected items

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ my_fruits_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/d
 my_fruits_list = my_fruits_list.set_index('Fruit')
 
 #let's put a pick list here so they can pick the fruits they want to include
-streamlit.multiselect('Pick some fruits:',list(my_fruits_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect('Pick some fruits:',list(my_fruits_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruits_list.loc[fruits_selected]
 
 #display the table on the page
-streamlit.dataframe(my_fruits_list)
+streamlit.dataframe(fruits_to_show)
