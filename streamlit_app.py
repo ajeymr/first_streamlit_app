00541977f181edd0259d5846c03f4baad30316d6
ajeymr <52828678+ajeymr@users.noreply.github.com>
2022-05-17 11:05:38 -0700

    Update streamlit_app.py
    added some pre populated items

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruits_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/d
 my_fruits_list = my_fruits_list.set_index('Fruit')
 
 #let's put a pick list here so they can pick the fruits they want to include
-streamlit.multiselect('Pick some fruits:',list(my_fruits_list.index))
+streamlit.multiselect('Pick some fruits:',list(my_fruits_list.index),['Avocado','Strawberries'])
 
 #display the table on the page
 streamlit.dataframe(my_fruits_list)
