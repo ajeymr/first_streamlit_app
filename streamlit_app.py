f5cabb770073e042283c2d5d0b1d4797b5a8cf30
ajeymr <52828678+ajeymr@users.noreply.github.com>
2022-05-19 18:27:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas as pd
 import requests
-import snowflake.connector
 
 streamlit.title('My Parents new Healthy Diner')
 
@@ -36,6 +35,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
